d6cc5df659fa6b73613cb293d44f2b7a47d49368
Juraj Oršulić <juraj.orsulic@fer.hr>
2023-10-16 02:02:49 +0200

    Update MousePositionPublisher

--- scripts/mouse_position_publisher.py
+++ scripts/mouse_position_publisher.py
@@ -5,27 +5,27 @@ from pynput.mouse import Controller
 import rospy
 from geometry_msgs.msg import Point
 
-class MousePositionPublisher:
 
+class MousePositionPublisher:
     def __init__(self):
         self.position_pub = rospy.Publisher("mouse_position", Point, queue_size=1)
-        self.position_msg = Point()
         self.mouse = Controller()
-        self.rate = rospy.Rate(10)
+        rospy.Timer(rospy.Duration(1.0 / 10.0), self.publish_mouse_position)
 
-    def run(self):
-        while not rospy.is_shutdown():
-            curr_position = self.mouse.position
-            self.position_msg.x = curr_position[0]
-            self.position_msg.y = curr_position[1]
-            self.position_pub.publish(self.position_msg)
-            self.rate.sleep()
+    def publish_mouse_position(self, _):
+        curr_position = self.mouse.position
+        position_msg = Point()
+        position_msg.x = curr_position[0]
+        position_msg.y = curr_position[1]
+        self.position_pub.publish(position_msg)
 
 
 def main():
-  rospy.init_node('mouse_position_publisher', anonymous=True)
-  mouse_position_publisher = MousePositionPublisher()
-  mouse_position_publisher.run()
+    rospy.init_node("mouse_position_publisher", anonymous=True)
+    mouse_position_publisher = MousePositionPublisher()
+    rospy.spin()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
+
